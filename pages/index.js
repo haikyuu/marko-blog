f1bd6223c2107fc75567db478ffe35b8f1e49bfe
Abdellah Alaoui <als.abdellah@gmail.com>
2021-02-16 09:30:46 +0100

    bug reproduction marko

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+class {
+    onCreate(){
+        this.state = {
+            isMenuOpen: false,
+        }
+    }
+    openMenu(){
+        console.log("open")
+        this.state.isMenuOpen = true;
+    }
+    closeMenu(){
+        console.log("close")
+        this.state.isMenuOpen = false;
+    }
+}
